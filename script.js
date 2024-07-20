e2e27fd34867b267351e64c8574a40487f566004
sanidhya-s-chandel <sanidhyasinghchandel31@gmail.com>
2024-07-21 00:17:25 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+function locomotive(){
+gsap.registerPlugin(ScrollTrigger);
+
+const locoScroll = new LocomotiveScroll({
+  el: document.querySelector(".smooth-scroll"),
+  smooth: true
+});
+
+locoScroll.on("scroll", ScrollTrigger.update);
+
+ScrollTrigger.scrollerProxy(".smooth-scroll", {
+  scrollTop(value) {
+    return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+  }, 
+  getBoundingClientRect() {
+    return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
+  },
+  
+  pinType: document.querySelector(".smooth-scroll").style.transform ? "transform" : "fixed"
+});
+ 
+ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+ScrollTrigger.refresh();
+}
